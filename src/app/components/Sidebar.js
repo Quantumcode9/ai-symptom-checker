77280ebf89db2e7127d622c48d6a2a4242974330
BrianTaylor <bataylor135@gmail.com>
2024-10-13 14:59:48 -0400

    Update Sidebar.js

--- src/app/components/Sidebar.js
+++ src/app/components/Sidebar.js
@@ -13,10 +13,6 @@ const bodyPartData = symptomsData.symptoms.find(
     (item) => item.name === bodyPart
 );
 
-if (!bodyPartData) {
-    return null;
-}
-
 const handleClickOutside = (event) => {
     if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
     onClose();
@@ -30,46 +26,50 @@ useEffect(() => {
     };
 }, []);
 
+if (!bodyPartData) {
+    return null;
+}
+
 return (
-<div
+    <div
     ref={sidebarRef}
     className="fixed top-20 right-0 w-64 h-auto max-h-[70vh] bg-sideBar shadow-lg z-50 mb-10"
->
+    >
     <div className="p-4">
-    <div className="flex justify-between items-center mb-2">
-    <h2 className="text-xl font-semibold mb-2">{bodyPartData.name}</h2>
-    <button onClick={onClose} className="text-red-500">
-        X
-    </button>
-    </div>
-    <div className="overflow-y-auto max-h-[60vh]">
+        <div className="flex justify-between items-center mb-2">
+        <h2 className="text-xl font-semibold mb-2">{bodyPartData.name}</h2>
+        <button onClick={onClose} className="text-red-500">
+            X
+        </button>
+        </div>
+        <div className="overflow-y-auto max-h-[60vh]">
         <ul>
-        {bodyPartData.symptoms
+            {bodyPartData.symptoms
             .slice()
-            .sort((a, b) => a.name.localeCompare(b.name)) 
+            .sort((a, b) => a.name.localeCompare(b.name))
             .map((symptom) => {
-            const isSelected = selectedSymptoms.some(
+                const isSelected = selectedSymptoms.some(
                 (item) =>
-                item.symptom === symptom.name && item.bodyPart === bodyPart
-            );
-            return (
+                    item.symptom === symptom.name && item.bodyPart === bodyPart
+                );
+                return (
                 <li
-                key={symptom.id}
-                className={`mb-2 p-2 cursor-pointer text-sm sm:text-base transition-colors duration-200 ease-in-out ${
+                    key={symptom.id}
+                    className={`mb-2 p-2 cursor-pointer text-sm sm:text-base transition-colors duration-200 ease-in-out ${
                     isSelected
-                    ? 'bg-blue-200 text-blue-800 font-semibold'
-                    : 'bg-gray-100 text-gray-800'
-                } hover:bg-blue-300`}
-                onClick={() => handleSymptomClick(symptom.name, bodyPart)}
+                        ? 'bg-blue-200 text-blue-800 font-semibold'
+                        : 'bg-gray-100 text-gray-800'
+                    } hover:bg-blue-300`}
+                    onClick={() => handleSymptomClick(symptom.name, bodyPart)}
                 >
-                {symptom.name}
+                    {symptom.name}
                 </li>
-            );
+                );
             })}
         </ul>
+        </div>
     </div>
     </div>
-</div>
 );
 };
 
